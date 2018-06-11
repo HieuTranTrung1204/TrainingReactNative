3099421dabb2386459c8c47f679002780bbff9e6
Barry0501 <laptung051@gmail.com>
2018-06-11 18:34:37 +0700

    + Kiến thức từ Skylab

--- App.js
+++ App.js
@@ -9,30 +9,59 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
+  AppRegistry,
+  Animated,
+  Easing
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component{
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      marginTopAnim: new Animated.Value(0), // Initial value
+      scaleAnim: new Animated.Value(1),
+    }
+  }
+
+  componentDidMount(){
+    Animated.timing(
+      this.state.marginTopAnim,
+      {
+        duration: 400,
+        toValue: 300,
+        easing: Easing.bezier(1, 0, 0, 1)        
+      }
+    ).start();
+
+    Animated.timing(
+      this.state.scaleAnim,
+      {
+        duration: 400,
+        toValue: 0.5,
+        easing: Easing.linear,
+        delay: 1000,
+      }
+    ).start();
+  }
+  
   render() {
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Animated.View 
+          style = {{
+            backgroundColor: 'aqua',
+            width: 100,
+            height: 100,
+            borderRadius: 50,
+            marginTop: this.state.marginTopAnim,
+            transform: [ {scale: this.state.scaleAnim } ]
+          }}
+        />
       </View>
     );
   }
@@ -41,18 +70,7 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    
   },
 });
+
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # TrainingReactNative
-Training ReactNative
+Thao tác với Animation
+- Trong React-Native có 2 hệ thống thao tác với Animation:
+    1. Animated
+    2. Layout Animation
+
+- There main methods to create Animation
+  + Animated.timing(): Create an animation with mappting time range to an easing value.
+
+  + Animated.decay(): Create an animation starts with an initial velocity and gradually slows to a stop.
+    . velocity: Initial velocity ( Required )
+    . deceleration: Default 0.997
+
+  + Animated.spring(): Create an animation with physical model by using velocity to create fluid motions as the toValue updates.
+
+
+1. Animated:
+  + toValue: Animation value will update yo
+  + duration: Time of anim ( Default: 500 ) (ms)
+  + easing: Default is easeInOut (See more: https://easings.net/)
+  + delay: Start the Anim after delay ( Default: 0 ) (ms)  
+  + useNativeDriver: Uses the native driver when true. Default is false
