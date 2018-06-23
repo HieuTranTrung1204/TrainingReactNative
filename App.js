04b1d147c9f8ae038dd26c8abe823ea0a67cc41a
Barry0501 <laptung051@gmail.com>
2018-06-23 19:38:36 +0700

    >.< Error khi sử dụng Lib có Animated

--- App.js
+++ App.js
@@ -12,12 +12,28 @@ import {
   View
 } from 'react-native';
 
+var FloatingLabel = require('react-native-floating-labels');
+
+// import { Sae } from 'react-native-textinput-effects';
+// import Icon from 'react-native-vector-icons/FontAwesome';
+// import { Fumi } from 'react-native-textinput-effects';
 
 export default class App extends Component{
   render() {
     return (
       <View style={styles.container}>
-        
+        <FloatingLabel 
+            labelStyle={styles.labelInput}
+            inputStyle={styles.input}
+ 
+            style={styles.formInput}
+          >First Name</FloatingLabel>
+        <FloatingLabel
+            labelStyle={styles.labelInput}
+            inputStyle={styles.input}
+            style={styles.formInput}
+            underlineColorAndroid={'#2AB57B'}
+          >Last Name</FloatingLabel>
       </View>
     );
   }
@@ -26,7 +42,18 @@ export default class App extends Component{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5FCFF',
+    paddingTop: 65,
+    backgroundColor: 'white',
   },
-  
+  labelInput: {
+    color: '#2AB57B',
+  },
+  formInput: {    
+    //borderBottomWidth: 1.5, 
+    marginLeft: 20,
+    borderColor: '#333',       
+  },
+  input: {
+    borderWidth: 0
+  }
 });
