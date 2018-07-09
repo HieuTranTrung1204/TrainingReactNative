d5191f00f9e98664cd9e07d20e11b699178d3d2a
Barry0501 <laptung051@gmail.com>
2018-07-09 20:27:50 +0700

    + Dropdown Tháng Ngày nhưng chưa xử lý đc validation

--- App.js
+++ App.js
@@ -12,13 +12,77 @@ import {
   View
 } from 'react-native';
 
+import { Dropdown } from 'react-native-material-dropdown';
 
 export default class App extends Component{
+  
+  constructor(props){
+    super(props);
+
+  }
+
+  _onChangText(data){
+    alert(data);
+  }
+  
   render() {
+    let data = [
+      {
+      value: 'January',
+      }, 
+      {
+        value: 'February',
+      }, 
+      {
+        value: 'March',
+      },
+      {
+        value: 'April',
+      },
+      {
+        value: 'May',
+      },
+      {
+        value: 'June',
+      },
+      {
+        value: 'July',
+      },
+      {
+        value: 'August',
+      },
+      {
+        value: 'September',
+      },
+      {
+        value: 'October',
+      },
+      {
+        value: 'November',
+      },
+      {
+        value: 'December',
+      },
+    ];
+
     return (
       <View style={styles.container}>
-        
+        <Dropdown
+          label='Month'
+          data={data}
+          containerStyle={{width:200,marginLeft: 10,}}
+          onChangeText={this._onChangText.bind(this)}
+          //dropdownMargins={{ min: 8, max: 16 }}
+          //shadeOpacity={0.25}
+        />
+        <Dropdown
+          label='Day'
+          data={data}
+          containerStyle={{width:100,marginLeft: 10,}}
+          onChangeText={this._onChangText.bind(this)}
+        />
       </View>
+      
     );
   }
 }
@@ -27,6 +91,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F5FCFF',
+    justifyContent:'center',
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   
 });
